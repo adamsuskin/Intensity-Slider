777c6a6a0b1fefcab9f10c47dc99ac9ae3825098
Adam Suskin <adamsuskin@gmail.com>
2015-10-11 19:18:15 -0400

    Created initial view for slider
    Updated main storyboard to contain slider… Will begin implementation
    now. Reset to here to clear all code and start fresh.

---
+++ Intensity Slider/Intensity Slider/IntensitySliderView.swift
@@ -0,0 +1,21 @@
+//
+//  IntensitySliderView.swift
+//  Intensity Slider
+//
+//  Created by Adam Suskin on 10/11/15.
+//  Copyright © 2015 Adam Suskin. All rights reserved.
+//
+
+import UIKit
+
+class IntensitySliderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
